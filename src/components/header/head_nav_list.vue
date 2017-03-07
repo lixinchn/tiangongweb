915ac0597114415000e50d92ffa490f626af4de2
lixin <lixinit@gmail.com>
2017-03-08 06:59:48 +0800

    head_nav_list

--- src/components/header/head_nav_list.vue
+++ src/components/header/head_nav_list.vue
@@ -6,6 +6,7 @@
         <li><a href="dataset">数据集</a></li>
         <li><a href="about_us">关于我们</a></li>
         <li><a href="races">测评</a></li>
+        <li v-show="loginShow"><a v-bind:href="loginHref">{{loginText}}</a></li>
       </ul>
     </div>
 
@@ -23,18 +24,40 @@
 </template>
 
 <script type="es6">
+  import {conf} from '../../assets/js/conf'
+
   export default {
-    name:'head-nav-list',
-    data(){
+    name: 'head-nav-list',
+    data() {
       return {
         visibility: 'initial',
         display: 'none',
+        loginHref: '',
+        loginText: '',
+        loginShow: false,
       }
+    },
+
+    created() {
+      this.$http.get(conf.host + '/user/info').then(response => {
+        let result = response.body
+        this.loginShow = true
+        if (result.code) {
+          this.loginHref = 'login'
+          this.loginText = '登录'
+          return
+        }
+
+        let userinfo = result.data
+        this.loginHref = 'user_info'
+        this.loginText = userinfo.phone
+      }, response => {
+        console.log(response)
+      })
     },
 
     methods: {
       toggleDropdown() {
-        console.log(this.visibility)
         if (this.visibility == 'initial') {
           this.visibility = 'visible'
           this.display = 'block'
