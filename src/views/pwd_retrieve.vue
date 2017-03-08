b801aaf723e3ac23dae1b6a3b73fc891e8136b6f
lixin <lixinit@gmail.com>
2017-03-09 00:49:46 +0800

    pwd_retrieve

--- src/views/pwd_retrieve.vue
+++ src/views/pwd_retrieve.vue
@@ -9,18 +9,34 @@
           <label for="phone">手机号：</label>
           <div style="clear: both;"></div>
           <input id="phone" v-model="user.phone" type="text" placeholder="">
-          <button v-on:click.prevent="onGetVerificationCode" type="submit" v-bind:class="{'code-gray': isGray}" class="pure-button pure-button-primary val-code" style="font-size: 1em;">{{valBtn}}</button>
+          <button v-on:click.prevent="onGetVerificationCode" type="button" v-bind:class="{'code-gray': isGray}" class="pure-button pure-button-primary val-code" style="font-size: 1em;">{{valBtn}}</button>
+          <div style="clear: both;"></div>
+        </div>
+
+        <div>
+          <label for="password">新密码：</label>
+          <div style="clear: both;"></div>
+          <input id="password" v-model="user.password" type="password" placeholder="">
+          <div style="clear: both;"></div>
+        </div>
+
+        <div>
+          <label for="passwordConfirm">新密码确认：</label>
+          <div style="clear: both;"></div>
+          <input id="passwordConfirm" v-model="user.passwordConfirm" type="password" placeholder="">
+          <div style="clear: both;"></div>
         </div>
 
         <div>
           <label for="verificationCode">验证码：</label>
           <div style="clear: both;"></div>
           <input id="verificationCode" v-model="user.verificationCode" type="text" placeholder="">
+          <div style="clear: both;"></div>
         </div>
 
         <div style="clear: both;"></div>
         <p v-if="error" class="retr-error">{{error}}</p>
-        <button type="submit" class="pure-button pure-button-primary">找回密码</button>
+        <button type="submit" class="pure-button pure-button-primary pwd-retr-sub">找回密码</button>
       </div>
     </fieldset>
   </form>
@@ -28,6 +44,7 @@
 </template>
 
 <script type="es6">
+  import {conf} from '../assets/js/conf'
   export default {
     methods: {
       onGetVerificationCode() {
@@ -71,7 +88,14 @@
       },
 
       getVerificationCode() {
-        // TODO
+        let formData = new FormData()
+        formData.append('phone', this.user.phone)
+        this.$http.post(conf.host + '/user/randcode', formData).then(response => {
+          console.log(response)
+        }, response => {
+          console.log(response)
+          this.error = '发生错误，请稍后再试'
+        })
       },
 
       changeBtnText(text) {
@@ -82,10 +106,21 @@
         if (!this.errorCheck())
           return
 
-        this.$http.post('/user/restpwd', this.user).then(response => {
-          console.log(response)
+        let formData = new FormData()
+        formData.append('phone', this.user.phone)
+        formData.append('password', this.user.password)
+        formData.append('randcode', this.user.verificationCode)
+        this.$http.post('/user/restpwd', formData).then(response => {
+          let result = response.body
+          if (result.code) {
+            this.error = result.msg
+            return
+          }
+
+          alert('密码重置成功，请重新登录')
+          location.href = '/login'
         }, response => {
-          console.log(response)
+          this.error = '发生错误，请稍后再试'
         })
       },
 
@@ -101,6 +136,21 @@
           return false
         }
 
+        if (!this.user.password || !this.user.password.trim()) {
+          this.error = '密码不能为空'
+          return false
+        }
+
+        if (this.user.password !== this.user.passwordConfirm) {
+          this.error = '两次输入的密码必须一致'
+          return false
+        }
+
+        if (this.user.password.length < 6) {
+          this.error = '密码必须大于等于6位'
+          return false
+        }
+
         return true
       },
     },
@@ -110,6 +160,8 @@
         user: {
           phone: null,
           verificationCode: null,
+          password: '',
+          passwordConfirm: '',
         },
         error: null,
         valBtn: '发送验证码',
@@ -159,12 +211,36 @@
 }
 
 #retr-form .code-gray {
-  background-color: #C7C7C7;
-  border: 2px solid #C7C7C7;
+  background-color: #C7C7C7 !important;
+  border: 2px solid #C7C7C7 !important;
 }
 
 #retr-form button {
   font-size: 1.2em;
+}
+
+#retr-form .val-code {
+  width: 20%;
+  display: inline-block;
+  margin-left: 2%;
+  min-width: 6em;
+
+  height: 50px;
+  line-height: 48px;
+  border: 2px solid #47b475;
+  background: #47b475;
+  color: #fff;
+  font-size: 11px;
+  text-transform: uppercase;
+  font-weight: bold;
+  letter-spacing: 1px;
+  border-radius: 0 !important;
+}
+
+.pwd-retr-sub {
+  width: 75% !important;
+  float: left !important;
+  margin-top: 2em !important;
 }
 
 </style>
